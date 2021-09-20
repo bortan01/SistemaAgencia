2910d46391996cab8070bf7814a4607eef4a1aaa
Abigail <pineverdi@gmail.com>
2021-09-20 16:43:13 -0600

    quitando home a pantallas

--- vistas/Asesoria/registroMigratoria.php
+++ vistas/Asesoria/registroMigratoria.php
@@ -32,7 +32,7 @@ include_once  '../../plantillas/navbar.php';
                </div>
                <div class="col-sm-6">
                   <ol class="breadcrumb float-sm-right">
-                     <li class="breadcrumb-item"><a href="#">Home</a></li>
+                     <li class="breadcrumb-item"><a href="#">Inicio</a></li>
                      <li class="breadcrumb-item active">Información Migratoria</li>
                   </ol>
                </div>
