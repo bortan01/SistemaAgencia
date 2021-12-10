f380a32b69aded28e06b74ec2e9265c70b94e366
Boris Ricardo Miranda <brmiranda009@gmail.com>
2021-12-10 15:19:55 -0600

    agregando feedback para la sesion

--- vistas/session/isEmpleado.php
+++ vistas/session/isEmpleado.php
@@ -5,7 +5,7 @@ if (
    isset($_SESSION["nivelA"]) &&
    ($_SESSION["nivelA"] == 'ADMINISTRADOR' || $_SESSION["nivelA"] == 'EMPLEADO')
 ) {
-   // echo ('<p style="padding-left: 265px;" >LOGUEADO, ROL ' . $_SESSION['nivelA'] . '</p>');
+   echo ('<p style="padding-left: 265px;" >LOGUEADO, ROL ' . $_SESSION['nivelA'] . '</p>');
 } else {
    header('Location:'.$base_url);
 }
